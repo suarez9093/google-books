d9e1ee03c433c41f1d4e39e988ce7f1f242847eb
Alex <suarez9093@hotmail.com>
2019-07-31 23:21:12 -0600

    adding routes to App component. Follow react-router Solved20 versionto make work

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
 import Results from "./components/Results";
+import SavedBooks from "./components/SavedBooks";
 
-function App(){
-    return(
-        <div>
-            <Navbar />
-            <Jumbotron />
-            <Results />
-        </div>
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <Navbar />
+                <Jumbotron />
+                <Switch>
+                    <Route exact path="/" component={Results} />
+                    <Route exact path="/saved" component={SavedBooks} />
+                </Switch>
+
+            </div>
+        </Router>
     )
 }
 
--- client/src/components/SavedBooks.js
+++ client/src/components/SavedBooks.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Results(){
+function SavedBooks(){
     return(
-        <div></div>
+        <h1>SavedBooks</h1>
     )
 }
-export default Results;
+
+export default SavedBooks;
