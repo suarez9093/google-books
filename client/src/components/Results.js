05bd9e5f6b8a5c1353670a64c2f64f743a00c027
Alex <suarez9093@hotmail.com>
2019-08-01 23:02:23 -0600

    working on the axios to get the data from the google API

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, {Component} from "react";
+import API from "./utils/API";
 import { Link } from "react-router-dom";
 
-function Results(){
-    return(
-        <div></div>
-    )
+class Results extends Component{
+    constructor(){
+        super()
+        this.state = {
+            books: [],
+            title: "",
+            author: "",
+            description: ""
+        };
+    }
+
+componentDidMount() {
+    this.loadBooks();
 }
+
+loadBooks = () => {
+    API.getbooks().then(res => 
+        this.setState({books: res.data, title: "", author: "", description: ""})
+
+    )
+    .catch(err =>console.log(err));
+}
+
+
+    render() {
+        return(
+            <div>Searched Books</div>
+        )
+    }
+    }
+   
 export default Results;
